c474410e1a3089c1b697c7e67cfd782e236fe3fc
Belhajamor Ahmed <36420506+belhajamoraahmed@users.noreply.github.com>
2021-06-15 14:13:13 +0200

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,56 +1,57 @@
-INSERT INTO planete   (ID,DESCRIPTION, NOM) VALUES  (1, 'est la planète la plus proche du Soleil, ainsi que la plus petite (4 878
+INSERT INTO planete   (ID,DESCRIPTION, NOM,url) VALUES  (1, 'est la planète la plus proche du Soleil, ainsi que la plus petite (4 878
 km de diamètre) et la moins massive avec un peu plus du vingtième de la
 masse terrestre
-','Mercure') ;
+','Mercure','http://assets.stickpng.com/thumbs/580b585b2edbce24c47b2708.png') ;
 
-INSERT INTO planete   (ID,DESCRIPTION, NOM) VALUES  (2, 'est la planète la plus proche de la Terre en taille (0,95 rayon terrestre)
+INSERT INTO planete   (ID,DESCRIPTION, NOM,url) VALUES  (2, 'est la planète la plus proche de la Terre en taille (0,95 rayon terrestre)
 et en masse (0,815 masse terrestre), faisant qu''elle est parfois appelée sa «
 planète sœur ». Comme elle, Vénus possède également un épais manteau
 de silicate entourant un noyau métallique, une atmosphère significative et une
 activité géologique interne
-','Vénus') ;
+','Vénus','http://assets.stickpng.com/thumbs/580b585b2edbce24c47b2708.png') ;
 
-INSERT INTO planete   (ID,DESCRIPTION, NOM) VALUES  (3, 'est la plus grande (12 756 km de diamètre), la plus massive et la plus
+INSERT INTO planete   (ID,DESCRIPTION, NOM,url) VALUES  (3, 'est la plus grande (12 756 km de diamètre), la plus massive et la plus
 dense des planètes du Système solaire. Elle est notamment le seul objet
 céleste connu pour abriter la vie.
-','Terre') ;
+','Terre','http://assets.stickpng.com/thumbs/580b585b2edbce24c47b2708.png') ;
 
-INSERT INTO planete   (ID,DESCRIPTION, NOM) VALUES  (4, 'est deux fois plus petite que la Terre et Vénus, et fait seulement environ
+INSERT INTO planete   (ID,DESCRIPTION, NOM,url) VALUES  (4, 'est deux fois plus petite que la Terre et Vénus, et fait seulement environ
 le dixième de la masse terrestre. Sa période de révolution autour du Soleil est
 de 687 jours terrestres et sa journée dure 24 heures et 39 minutes
-','Mars') ;
+','Mars','http://assets.stickpng.com/thumbs/580b585b2edbce24c47b2708.png') ;
 
-INSERT INTO planete   (ID,DESCRIPTION, NOM) VALUES  (5, 'avec 318 masses terrestres, est aussi massive que 2,5 fois toutes
+INSERT INTO planete   (ID,DESCRIPTION, NOM,url) VALUES  (5, 'avec 318 masses terrestres, est aussi massive que 2,5 fois toutes
 les autres planètes réunies et son diamètre avoisine les 143 000 kilomètres.
 Sa période de révolution est d''environ 12 ans et sa période de rotation est
 d''un peu moins de 10 heures.
-','Jupiter') ;
+','Jupiter','http://assets.stickpng.com/thumbs/580b585b2edbce24c47b2708.png') ;
 
-INSERT INTO planete   (ID,DESCRIPTION, NOM) VALUES  (6, 'possède des caractéristiques similaires à Jupiter, telles que sa
+INSERT INTO planete   (ID,DESCRIPTION, NOM,url) VALUES  (6, 'possède des caractéristiques similaires à Jupiter, telles que sa
 composition atmosphérique et sa puissante magnétosphère. Bien qu''elle
 fasse 60 % du volume de l''autre planète géante gazeuse du fait de son
 diamètre équatorial d''environ 121 000 kilomètres, elle est beaucoup moins
 massive avec 95 masses terrestres.
-','Saturne') ;
+','Saturne','http://assets.stickpng.com/thumbs/580b585b2edbce24c47b2708.png') ;
 
 
-INSERT INTO planete   (ID,DESCRIPTION, NOM) VALUES  (7, 'est la moins massive des planètes géantes avec 14 masses
+INSERT INTO planete   (ID,DESCRIPTION, NOM,url) VALUES  (7, 'est la moins massive des planètes géantes avec 14 masses
 terrestres. Avec son diamètre d''environ 51 000 kilomètres, elle demeure plus
 grande que sa presque jumelle Neptune en raison de la compression
 gravitationnelle de cette dernière
-','Uranus') ;
+','Uranus','http://assets.stickpng.com/thumbs/580b585b2edbce24c47b2708.png') ;
 
-INSERT INTO planete   (ID,DESCRIPTION, NOM) VALUES  (8, ' est la planète la plus éloignée du Soleil connue au sein du Système
+INSERT INTO planete   (ID,DESCRIPTION, NOM,url) VALUES  (8, ' est la planète la plus éloignée du Soleil connue au sein du Système
 solaire. Légèrement plus massive qu''Uranus avec 17 masses terrestres, elle
 est par conséquent plus dense avec un diamètre équatorial d''environ 49 500
 kilomètres par compression gravitationnelle.
-','Neptune') ;
+','Neptune','http://assets.stickpng.com/thumbs/580b585b2edbce24c47b2708.png') ;
 
-INSERT INTO planete   (ID,DESCRIPTION, NOM) VALUES  (9, 'a perdu son statut de neuvième planète du Système solaire. A la
+INSERT INTO planete   (ID,DESCRIPTION, NOM,url) VALUES  (9, 'a perdu son statut de neuvième planète du Système solaire. A la
 suite d’un vote de l’Union astronomique internationale, elle s’est vue
 rétrogradée en planète naine. La raison principale de ce changement
 d’appellation provient d’une redéfinition de ce qu’est une planète.
-','Pluton') ;
+','Pluton','http://assets.stickpng.com/thumbs/580b585b2edbce24c47b2708.png') ;
 
 INSERT INTO user(id ,email, mdp,groupe,username) VALUES(1,"mariem@gmail.com","123","2ing2","mariem hajjem");
+INSERT INTO user(id ,email, mdp,groupe,username) VALUES(1,"ahmed@gmail.com","123","2ing1","Ahmed ahmed");
 
